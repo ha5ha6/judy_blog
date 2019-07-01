7c5dbb1072ad348ef53eb4b0a0f5b67ad3ea2890
wang-j <wang-j@wanwan-wl.cns.atr.jp>
2019-07-01 11:11:45 +0900

    “2”

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,5 +2,5 @@ var store = [{
         "title": "Test Post",
         "excerpt":"You’ll find this post in your `_posts` directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run `jekyll serve`, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
         "tags": [],
-        "url": "/judy1/jekyll/update/2019/03/26/welcome-to-jekyll.html",
-        "teaser":"/judy1/assets/images/hand.png"}]
+        "url": "http://localhost:4000/judy_blog/jekyll/update/2019/03/26/welcome-to-jekyll.html",
+        "teaser":"http://localhost:4000/judy_blog/assets/images/hand.png"}]
